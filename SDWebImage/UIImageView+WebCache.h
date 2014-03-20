34557776422cde62ff8a7a6d539ff102fa65dd71
Anil Anar <aanar@videa.com.tr>
2014-03-20 14:25:19 +0200

    Minor typo.

--- SDWebImage/UIImageView+WebCache.h
+++ SDWebImage/UIImageView+WebCache.h
@@ -144,7 +144,7 @@
  * @param cacheKey The key that will be used to cache the image.
  * @param options The options to use when downloading the image. @see SDWebImageOptions for the possible values.
  * @param progressBlock A block called while image is downloading
- * @param completedBlock A block called when operation has been completed. This block as no return value
+ * @param completedBlock A block called when operation has been completed. This block has no return value
  *                       and takes the requested UIImage as first parameter. In case of error the image parameter
  *                       is nil and the second parameter may contain an NSError. The third parameter is a Boolean
  *                       indicating if the image was retrived from the local cache of from the network.
